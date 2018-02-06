8f2215b0a3dd36ecb802d9720f082bf88db94732
pedersee7734 <36180429+pedersee7734@users.noreply.github.com>
2018-02-06 03:27:20 +0000

    Add files via upload
    Second

---
+++ P2HW1_DistanceTraveled_Pedersen,Erik.py
@@ -0,0 +1,9 @@
+#CTI-110
+# P2HW1
+# Erik Pedersen
+# 3 Feb 2018
+
+speed = 70
+print(" Distance after 6 hours: " + str ( ( speed * 6 )) + " miles")
+print(" Distance after 10 hours: " + str ( ( speed * 10 )) + " miles")    
+print(" Distance after 15 hours: " + str ( ( speed * 15 )) + " miles")
