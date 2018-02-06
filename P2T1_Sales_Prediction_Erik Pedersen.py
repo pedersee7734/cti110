c849ffa8a7f121a13e1ec9037e28b023af961ee4
pedersee7734 <36180429+pedersee7734@users.noreply.github.com>
2018-02-06 03:28:02 +0000

    Add files via upload
    4th

---
+++ P2T1_Sales_Prediction_Erik Pedersen.py
@@ -0,0 +1,14 @@
+# CTI-110
+# P2T1 - Sales Prediction
+# Erik Pedersen
+# 3 Feb 2018
+
+#Get the projected sales.
+total_sales = float(input('Enter the total sales: '))
+
+# Calculate the profit as 23 percent of total sales.
+annual_profit = total_sales * 0.23
+
+# Display the profit.
+print ('The annual profit is $', format(annual_profit, ',.2f'))
+       
