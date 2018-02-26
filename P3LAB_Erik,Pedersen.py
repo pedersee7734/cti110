258eee161dc2c65868bf9fd961fa96227103c366
pedersee7734 <36180429+pedersee7734@users.noreply.github.com>
2018-02-26 02:48:46 +0000

    Grades

---
+++ P3LAB_Erik,Pedersen.py
@@ -0,0 +1,21 @@
+#Erik Pedersen
+#CTI-110
+#Debugging
+#2/25/2018
+
+
+score = int (input ('Enter grade: '))
+ 
+if score >= 90: 
+    print('Your grade is: A')
+elif score >= 80 :
+     print('Your grade is: B')
+elif score >= 70 :
+    print (" Your grace is: C")
+elif score >= 60 :
+    print (" Your grade is: D")
+elif score <= 59 :
+    print('Your grade is: F')
+
+
+
